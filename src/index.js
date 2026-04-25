a59d2013864e94805e4b2828f3fbef5e7cbebd59
yassforces-del <yassforces@gmail.com>
2026-04-25 09:04:51 +0100

    fix: cleanup gitignore remove compiled files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_server_1 = require("@hono/node-server");
-const app_1 = require("./app");
-(0, node_server_1.serve)({
-    fetch: app_1.app.fetch,
-    port: 3000
-});
-console.log('🚀 Quant Risk Engine ready');
